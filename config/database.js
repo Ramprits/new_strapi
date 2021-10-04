440d16710f0b8ffcf091d6af84fb0ca8ee202e95
Ramprit Sahani <ramprit@Ramprits-MacBook-Air.local>
2021-10-04 09:38:02 +0530

    Adding all required models

--- config/database.js
+++ config/database.js
@@ -1,26 +1,44 @@
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "bookshelf",
-        settings: {
-          client: "postgres",
-          host,
-          port,
-          database,
-          username: user,
-          password,
-          ssl: { rejectUnauthorized: false },
+  if (process.env.NODE_ENV !== "production") {
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          settings: {
+            client: "sqlite",
+            filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+          },
+          options: {
+            useNullAsDefault: true,
+          },
         },
-        options: {},
       },
-    },
-  };
+    };
+  } else {
+    const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          settings: {
+            client: "postgres",
+            host,
+            port,
+            database,
+            username: user,
+            password,
+            ssl: { rejectUnauthorized: false },
+          },
+          options: {},
+        },
+      },
+    };
+  }
 };
 
 // Use this configuration for an SQLite database on your machine.
